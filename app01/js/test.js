a895e8c107d86eded650db85641db549f71f3054
drsanti <santi.inc.kmutt@gmail.com>
2019-01-30 16:08:08 +0700

    update

--- app01/js/test.js
+++ app01/js/test.js
@@ -1,10 +1,19 @@
 
-import {scene, camera, renderer, box} from './init';
+import {scene, camera, renderer, box, wall1Mesh, wall2Mesh, floorMesh} from './init';
 import * as THREE from 'three';
 
-var texture = new THREE.TextureLoader().load( 'images/brick.jpg' );
-var material = new THREE.MeshBasicMaterial( { map: texture } );
-console.log(material);
+var brickTexture = new THREE.TextureLoader().load( 'images/brick.jpg' );
+var brickMaterial = new THREE.MeshPhongMaterial( { map: brickTexture } );
+
+box.material       = brickMaterial;
+wall1Mesh.material = brickMaterial;
+wall2Mesh.material = brickMaterial;
+floorMesh.material = brickMaterial;
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}, false);
 
 function animate() {
 	requestAnimationFrame( animate );
