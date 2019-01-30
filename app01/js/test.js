76a46f929d05d45b0b097eaeee10535a8d402834
drsanti <santi.inc.kmutt@gmail.com>
2019-01-30 17:20:19 +0700

    update

--- app01/js/test.js
+++ app01/js/test.js
@@ -10,6 +10,11 @@ brickTexture.wrapT = THREE.RepeatWrapping;
 brickTexture.repeat.set( 4, 4 );
 
 
+box.material       = brickMaterial;
+wall1Mesh.material = brickMaterial;
+wall2Mesh.material = brickMaterial;
+floorMesh.material = brickMaterial;
+
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
@@ -19,12 +24,20 @@ window.addEventListener('resize', () => {
 
 
 var btn = document.getElementById('btn');
+
+
+var boxes = [];
+var pos = 1;
 btn.addEventListener('click', function() {
-    box.material       = brickMaterial;
-    wall1Mesh.material = brickMaterial;
-    wall2Mesh.material = brickMaterial;
-    floorMesh.material = brickMaterial;  
+
+    var b = box.clone();
+    b.position.x = pos*1.5;  
+    pos++; 
+    boxes.push(b);
+    scene.add(b);
 });
+
+
 
 
 
@@ -32,11 +45,12 @@ btn.addEventListener('click', function() {
 function animate() {
 	requestAnimationFrame( animate );
     renderer.render( scene, camera );
-    box.rotation.z += 0.001;
-    box.rotation.y += 0.02;
-    box.rotation.x += 0.01;
 
-    brickTexture.offset.y += 0.001;
+    boxes.forEach(b => {
+        b.rotation.x += 0.01;
+    });
+
+
 }
 animate();
 
