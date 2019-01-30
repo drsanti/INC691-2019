3694e2a3f7a62b982218b963cac007b7240b3bce
drsanti <santi.inc.kmutt@gmail.com>
2019-01-30 17:45:40 +0700

    update

--- app01/js/test.js
+++ app01/js/test.js
@@ -24,8 +24,7 @@ window.addEventListener('resize', () => {
 
 
 var btn = document.getElementById('btn');
-
-
+/*
 var boxes = [];
 var pos = 1;
 btn.addEventListener('click', function() {
@@ -36,20 +35,46 @@ btn.addEventListener('click', function() {
     boxes.push(b);
     scene.add(b);
 });
+*/
+box.scale.x = 0.2;
+box.scale.y = 0.2;
+box.scale.z = 0.2;
+box.visible = false;
+
+
+var holder = new THREE.Object3D();
+
+
+var boxes = [];
+var POINTS = 50;
+for(var i=0; i< POINTS; i++) {
+    var b = box.clone();
+    b.visible = true;
+    b.position.x = i*0.1 - 4;
+    b.position.y =  2.2 + 2 * Math.sin(Math.PI * 2 * i / POINTS);
+    boxes.push(b);
+    holder.add(b); 
+}
 
 
 
 
 
+var t = 0;
+
+scene.add(holder);
 
 function animate() {
 	requestAnimationFrame( animate );
     renderer.render( scene, camera );
 
-    boxes.forEach(b => {
-        b.rotation.x += 0.01;
-    });
+    // boxes.forEach(b => {
+    //     b.rotation.x += 0.01;
+    //     b.position.y = 2.2 + 2 * Math.sin(t * Math.PI * 4);
+    // });
+    holder.rotation.y += 0.01;
 
+    boxes[7].rotation.y += 0.1;
 
 }
 animate();
