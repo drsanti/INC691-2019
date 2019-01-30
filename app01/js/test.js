f37e9feda78634f6f383ee86368286730f20d68f
drsanti <santi.inc.kmutt@gmail.com>
2019-01-30 16:39:42 +0700

    update

--- app01/js/test.js
+++ app01/js/test.js
@@ -2,8 +2,14 @@
 import {scene, camera, renderer, box, wall1Mesh, wall2Mesh, floorMesh} from './init';
 import * as THREE from 'three';
 
+
 var brickTexture = new THREE.TextureLoader().load( 'images/brick.jpg' );
 var brickMaterial = new THREE.MeshPhongMaterial( { map: brickTexture } );
+brickTexture.wrapS = THREE.RepeatWrapping;
+brickTexture.wrapT = THREE.RepeatWrapping;
+brickTexture.repeat.set( 4, 4 );
+
+
 
 box.material       = brickMaterial;
 wall1Mesh.material = brickMaterial;
@@ -15,12 +21,15 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }, false);
 
+
 function animate() {
 	requestAnimationFrame( animate );
     renderer.render( scene, camera );
     box.rotation.z += 0.001;
     box.rotation.y += 0.02;
     box.rotation.x += 0.01;
+
+    brickTexture.offset.y += 0.001;
 }
 animate();
 
