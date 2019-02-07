85abe228642c9e30bfa8648c17c0ed2863f2c0d7
drsanti <santi.inc.kmutt@gmail.com>
2019-02-07 12:15:43 +0700

    update

--- apps/ex-01/usr/index.js
+++ apps/ex-01/usr/index.js
@@ -1,13 +1,41 @@
-import {Engine} from './lib/ecc-cg-scene';
+import {Engine, CANNON} from './lib/ecc-cg-scene';
 
-let engine = new Engine({usePhysics: false});
 
+let engine = new Engine({usePhysics: true, fogColor: 0xaaaaaa});
+var body;   // physics
 engine.init({
     envPath:   'images/Park2/',
-    modelPath: 'models/box.gltf'
+    modelPath: 'models/hello.gltf'
 }).then( function(param){
-    engine.start();
+    engine.setGravity(new CANNON.Vec3(0, -9.8, 0));
+    body  = engine.getBodyByMeshName('Cube');
+    body.mass = 2;
+    engine.start( update );
+
+    engine.on('keydown', addForce);
+
 });
+
+function addForce(evt) {
+    if(evt.key == 'w') {
+        var p = new CANNON.Vec3(0, 0, 0);
+        var f = new CANNON.Vec3(0, 0, 1000);
+        body.applyForce(f, p);
+    }
+    else if(evt.key == 's') {
+        var p = new CANNON.Vec3(0, 0, 0);
+        var f = new CANNON.Vec3(0, 0, -1000);
+        body.applyForce(f, p);
+    }
+}
+
+function update ( deltaT, frameCnt ) {
+    
+}
+
+
+
+
 
 
 
