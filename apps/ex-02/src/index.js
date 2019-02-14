98cba026e2483fcee094b8fbbc278e9527d91102
drsanti <santi.inc.kmutt@gmail.com>
2019-02-14 12:08:22 +0700

    update

--- apps/ex-02/src/index.js
+++ apps/ex-02/src/index.js
@@ -1,43 +1,70 @@
-import { Engine, CANNON } from './libs/ecc-cgp-engine';
-const engine = new Engine({ sceneType: 'env'});
 
-engine.init( {envPath:'./images/Bridge2/'} ).then((params) => {
-    userInit(params);
-    engine.setGravity(new CANNON.Vec3(0, -9.8, 0));
-    engine.start();
+//!! Import Engine used classes
+import { Engine, CANNON, Keyboard } from './libs/ecc-cgp-engine';
+
+//!! Create the engine w/ simple scene
+const engine = new Engine({ 
+    sceneType: 'fog',       //!! 'env', 'fog', 'basic'
+    useReflection: false    //!! true, false
+});
+
+
+//!! Initialise the engine with the environment and initial scene
+engine.init( {
+    envPath:   './images/Bridge2/',         //!! images directory
+    modelPath: './models/container.gltf'    //!! model file name
+}).then((params) => {                       //!! params is an object containing graphics and physics objects
+    userInit( params );                     //!! user function used as initialized function
+    engine.setGravity(new CANNON.Vec3(0, -9.8, 0)); //!! set gravity vector pointing down (-y)
+    engine.start();                         //!! start the engine
 }); 
 
-var body = null;
+//!! 
+var body = null;        //!! Target
+var ground = null;       //!! Plane/Ground
 
+//!! User initial function
 function userInit( params ) {
+
+    //!! Target
     body = engine.getBodyByMeshName('Cube');
-    var ground = engine.getBodyByMeshName('StaticCube');
+
+    //!! Plane/Ground
+    ground = engine.getBodyByMeshName('StaticCube');
+    
+    //!! Ground Material
     var groundMat = engine.createGroundMaterial(0.200, 0.5);
+    
+    //!! Target object material
     var objectMat = engine.createObjectMaterial(0.001, 0.0, groundMat);
 
+    //!
     ground.material = groundMat;    /* Apply ground material 		*/
     body.material = objectMat;      /* Apply object material 		*/
-    body.mass = 0.2;			    /* Change mass of the object	*/
-    engine.on('keydown', onKeyDown);
-
-
-    var rot = new CANNON.Vec3(1,0,0);
-    ground.quaternion.setFromAxisAngle(rot,(Math.PI/20))
+    
+    body.mass = 1;			        /* Change mass of the object	*/
+    
 }
 
 
-function onKeyDown(event) {
-    if (event.key == 'f') {
-        applyForce();
-    }
-}
-/* Apply force vector to the world-point*/
-function applyForce() {
+//!! Apply force vector to the world-point
+function applyForce(dirx, diry, dirz) {
+
+    //!! Create a world point vector
     const worldPoint = new CANNON.Vec3(0, 0, 0);
-    const force = new CANNON.Vec3(15, 0, 0);
+
+    const fx = 200;     //!! Right-Left
+    const fy = 0;       //!! Jump (+y)
+    const fz = 0;       //!! Forward-Backward
+
+    //!! Create force vector
+    const force = new CANNON.Vec3(dirx*fx, diry*fy, dirz*fz);
+
+    //!! Apply the force to a point, pointed by the worldPoint vector
     body.applyForce(force, worldPoint);
 }
 
+//!!
 function callback(args) {
-
+    //!!
 }
