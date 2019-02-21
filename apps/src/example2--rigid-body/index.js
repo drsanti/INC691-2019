e09f475333ab0949a5a5a35d75a886cb43eaf965
drsanti <santi.inc.kmutt@gmail.com>
2019-02-21 10:25:43 +0700

    update

--- apps/src/example2--rigid-body/index.js
+++ apps/src/example2--rigid-body/index.js
@@ -1,31 +1,53 @@
-/**
- * Example02 -- Rigid Body
- */
 
- //!! Import the libraries
+//!! Import the libraries
 import { Engine, CANNON } from 'ecc-cgp-engine';
 
-//!! Create a new Engine
+//!! Create the engine
 const engine = new Engine();
 
 
 //!! Options
 const initOpts = {
-    models: ['models/scene.gltf'], //!! 
+    envPath: 'images/Bridge2',
+    models:[
+        'models/scene.gltf',
+    ],
 }
 
-//!! Initialse the ening
-engine.init(initOpts, function( args ){
-    //!! Check the target mesh name in the console window
+//!! Target (global variable)
+var target = undefined;
 
-    const meshName = 'Cube';
-
-    var rigidBody = engine.physics.bodyUtils.getBodyByMeshName( meshName );
-
-    console(rigidBody);
-
-    engine.start(); 
+//!! Initialise the engine
+engine.init(initOpts).then( (args) => {
+    userInit();
+    engine.start( loop );
 });
 
+//!! User funtion used to initilise our scene
+function userInit() {
+
+    //!! Target name, the mesh name
+    const meshName = 'Cube';
+
+    //!! Get the rigid body ftom the physics world
+    const rigidBody = engine.physics.bodyUtils.getBodyByMeshName( meshName );
+
+    //!! The taget will be used in the loop, see below
+    target = rigidBody;
+}
+
+
+
+//!! RAD
+var angle = 0.0;
+
+//!! Animation loop
+function loop() {
+
+    if( target ) {
+        target.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), angle);
+        angle+=0.02;
+    }
+}
 
 
