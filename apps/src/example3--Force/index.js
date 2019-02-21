24771a0579c50be5ba9867b065149e6c590b0136
drsanti <santi.inc.kmutt@gmail.com>
2019-02-21 11:03:34 +0700

    update

--- apps/src/example3--Force/index.js
+++ apps/src/example3--Force/index.js
@@ -36,6 +36,10 @@ function userInit() {
 
     //!! The taget will be used in the loop, see below
     target = rigidBody;
+
+    //
+    target.allowSpeep = false;
+    //engine.physics.world.allowSpeep = false;
 }
 
 
@@ -51,11 +55,25 @@ function loop() {
         const worldPoint = new CANNON.Vec3( 0, 0, 0 );
 
         //!! Force
-        const force = 100;
-        var forceVector = new CANNON.Vec3( force, 0, 0 );
+        const force = 500;
+        var forceVector = new CANNON.Vec3( 0, force, 0 );
 
         //!! Apply the forceVector to the worldPoint
         target.applyForce( forceVector, worldPoint );
+    }
+
+    if( engine.keyboard.getKeyDown( 'g' ) ) {
+
+        //!! Force location (the center of the target)
+        const targetPoint = new CANNON.Vec3( 0, 0, 0 );
+        targetPoint.clone(target.position);
+
+        //!! Force
+        const force = 50;
+        var forceVector = new CANNON.Vec3( 0, force, 0 );
+
+        //!! Apply the forceVector to the worldPoint
+        target.applyForce( forceVector, targetPoint );
     }
     
 }
