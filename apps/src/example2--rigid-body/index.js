31d185339279c0015bd6dfd003809f4a9b55e969
drsanti <santi.inc.kmutt@gmail.com>
2019-02-21 09:59:07 +0700

    ex2 updated

--- apps/src/example2--rigid-body/index.js
+++ apps/src/example2--rigid-body/index.js
@@ -2,15 +2,28 @@
  * Example02 -- Rigid Body
  */
 
+ //!! Import the libraries
 import { Engine, CANNON } from 'ecc-cgp-engine';
 
+//!! Create a new Engine
 const engine = new Engine();
 
+
+//!! Options
 const initOpts = {
-    models: ['models/scene.gltf'],
+    models: ['models/scene.gltf'], //!! 
 }
 
+//!! Initialse the ening
 engine.init(initOpts, function( args ){
+    //!! Check the mesh name(s)
+    console.log(args.graphics.scene);
 
+
+    const meshName = 'Cube';
+
+    engine.start(); 
 });
+
+
 
